e4a278dd78e7c2a017e34d5145ac132ddb9c10d2
gunwoo <gunwoo6752@gmail.com>
2020-06-23 23:28:00 +0900

    logback으로 기본적인 logging 설정

--- mysite/mysite/src/main/java/com/toy/mysite/MysiteApplication.java
+++ mysite/mysite/src/main/java/com/toy/mysite/MysiteApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MysiteApplication {
-
+			
 	public static void main(String[] args) {
 		SpringApplication.run(MysiteApplication.class, args);
 	}
-	
 	
 }
