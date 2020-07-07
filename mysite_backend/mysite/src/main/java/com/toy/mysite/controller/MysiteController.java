d01eedd5b2d024b9c69a0b62cbdf896afcb429a1
gunwoo <gunwoo6752@gmail.com>
2020-07-07 23:52:01 +0900

    axios로 Server 접근 테스트 진행중..

--- mysite_backend/mysite/src/main/java/com/toy/mysite/controller/MysiteController.java
+++ mysite_backend/mysite/src/main/java/com/toy/mysite/controller/MysiteController.java
@@ -1,13 +1,18 @@
 package com.toy.mysite.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MysiteController {
 
+	private static final Log log = LogFactory.getLog(MysiteController.class);
+	
 	@GetMapping("/axiosTest")
 	public String axiosTest() {
+		log.info("axiosTest Method Entered!");
 		return "Axios Test Success!!";
 	}
 	
---
+++ mysite_frontend/mysite/src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created(){
+        console.log('create function entered.');
+        axios.get('http://localhost:8080/axiosTest').then(response => console.log('gogo'));
+    }
+}
+</script>
